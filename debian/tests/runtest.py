6bc52c1df3bd567ed03067725446d6b3b6d73f51
David Steele <steele@debian.org>
2017-11-18 17:53:05 -0500

    Add autopkgtest

---
+++ debian/tests/runtest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import asyncio
+import aiofiles
+
+async def test_aiofiles():
+    async with aiofiles.open('README.rst', 'r') as fp:
+        contents = await fp.read()
+
+    assert('aiofiles' in contents)
+
+
+event_loop = asyncio.get_event_loop()
+try:
+    event_loop.run_until_complete(test_aiofiles())
+finally:
+    event_loop.close()
